69ddcc9d4390e5f2a316881f69acaaf3d4546001
darrenyaoyao <darrenyaoyao.huang@gmail.com>
2022-01-02 19:07:25 +0800

    training code

--- train.py
+++ train.py
@@ -44,3 +44,198 @@ print(f'Train files\n\tpath: {TRAIN_DIR}\n\ttotal number: {train_total}\n{train_
 print(f'Train files\n\tpath: {TEST_DIR}\n\ttotal number: {test_total}\n{test_text}')
 
 to_rgb_and_save(trainF), to_rgb_and_save(testF)
+
+from facenet_pytorch import MTCNN, InceptionResnetV1, training, fixed_image_standardization
+
+import tqdm
+
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+import numpy as np
+
+import shutil
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(f'Running on device: {device}')
+
+
+def crop_face_and_save(path, new_path=None, model=MTCNN, transformer=None, params=None):
+    """
+    Detect face on each image, crop them and save to "new_path"
+    :param str path: path with images will be passed to  datasets.ImageFolder
+    :param str new_path: path to locate new "aligned" images, if new_path is None
+                     then new_path will be path + "_cropped"
+    :param model: model to detect faces, default MTCNN
+    :param transformer: transformer object will be passed to ImageFolder
+    :param params: parameters of MTCNN model
+    """
+    if not new_path:
+        new_path = path + '_cropped'
+
+    # in case new_path exists MTCNN model will raise error
+    if os.path.exists(new_path):
+        shutil.rmtree(new_path)
+
+    # it is default parameters for MTCNN
+    if not params:
+        params = {
+            'image_size': 160, 'margin': 0,
+            'min_face_size': 10, 'thresholds': [0.6, 0.7, 0.7],
+            'factor': 0.709, 'post_process': False, 'device': device
+            }
+
+    model = model(**params)
+
+    if not transformer:
+        transformer = transforms.Lambda(
+            lambd=lambda x: x.resize((1280, 1280)) if (np.array(x) > 2000).all() else x
+        )
+    # for convenience we will use ImageFolder instead of getting Image objects by file paths
+    dataset = datasets.ImageFolder(path, transform=transformer)
+    dataset.samples = [(p, p.replace(path, new_path)) for p, _ in dataset.samples]
+
+    # batch size 1 as long as we havent exact image size and MTCNN will raise an error
+    loader = DataLoader(dataset, batch_size=1, collate_fn=training.collate_pil)
+    for i, (x, y) in enumerate(tqdm.tqdm(loader)):
+        model(x, save_path=y)
+
+    # spare some memory
+    del model, loader, dataset
+
+# 3. Crop train dataset faces and save aligned images
+print('\t- Train data')
+crop_face_and_save(TRAIN_DIR, ALIGNED_TRAIN_DIR)
+
+# check if some imgs were missed by detector and failed to save
+train_files, train_aligned_files = get_files(TRAIN_DIR), get_files(ALIGNED_TRAIN_DIR)
+if len(train_files) != len(train_aligned_files):
+    files = set(map(lambda fp: os.path.relpath(fp, start=TRAIN_DIR), train_files))
+    aligned_files = set(map(lambda fp: os.path.relpath(fp, start=ALIGNED_TRAIN_DIR), train_aligned_files))
+    detect_failed_train_files = list(files - aligned_files)
+    print(f"\nfiles {len(aligned_files)}/{len(files)}: {', '.join(detect_failed_train_files)} were not saved")
+
+# -------------                     -------------
+
+# Crop test dataset faces and save aligned images
+print('\t- Test data')
+crop_face_and_save(TEST_DIR, ALIGNED_TEST_DIR)
+
+# check if some imgs were missed by detector and failed to save
+test_files, test_aligned_files = get_files(TEST_DIR), get_files(ALIGNED_TEST_DIR)
+if len(test_files) != len(test_aligned_files):
+    files = set(map(lambda fp: os.path.relpath(fp, start=TEST_DIR), test_files))
+    aligned_files = set(map(lambda fp: os.path.relpath(fp, start=ALIGNED_TEST_DIR), test_aligned_files))
+    detect_failed_test_files = list(files - aligned_files)
+    print(f"\nfiles {len(aligned_files)}/{len(files)}: {', '.join(detect_failed_train_files)} were not saved")
+
+trainF = get_files(ALIGNED_TRAIN_DIR)
+testF = get_files(ALIGNED_TEST_DIR)
+
+import albumentations as A
+
+from facenet_pytorch import fixed_image_standardization
+
+standard_transform = transforms.Compose([
+                                np.float32,
+                                transforms.ToTensor(),
+                                fixed_image_standardization
+])
+
+aug_mask = A.Compose([
+                   A.HorizontalFlip(p=0.5),
+                   A.VerticalFlip(p=0.15),
+                   A.RandomContrast(limit=0.5, p=0.4),
+                   A.Rotate(30, p=0.2),
+                   A.RandomSizedCrop((120, 120), 160, 160, p=0.4),
+                   A.OneOrOther(A.JpegCompression(p=0.2), A.Blur(p=0.2), p=0.66),
+                   A.OneOf([
+                            A.Rotate(45, p=0.3),
+                            A.ElasticTransform(sigma=20, alpha_affine=20, border_mode=0, p=0.2)
+                            ], p=0.5),
+                  A.HueSaturationValue(val_shift_limit=10, p=0.3)
+], p=1)
+
+transform = {
+    'train': transforms.Compose([
+                                 transforms.Lambda(lambd=lambda x: aug_mask(image=np.array(x))['image']),
+                                 standard_transform
+    ]),
+    'test': standard_transform
+}
+
+from torch.utils.data import SubsetRandomSampler
+
+
+b = 32
+
+# Original train images
+trainD = datasets.ImageFolder(ALIGNED_TRAIN_DIR, transform=standard_transform)
+# Augmented train images
+trainD_aug = datasets.ImageFolder(ALIGNED_TRAIN_DIR, transform=transform['train'])
+# Train Loader
+trainL = DataLoader(trainD, batch_size=b, num_workers=2) # x: torch.Size([batch_size, 3, 160, 160]), y: torch.Size([batch_size])
+trainL_aug = DataLoader(trainD_aug, batch_size=b, num_workers=2)
+
+# Original test images
+testD = datasets.ImageFolder(ALIGNED_TEST_DIR, transform=standard_transform)
+# Test Loader
+testL = DataLoader(testD, batch_size=b, num_workers=2)
+
+# Convert encoded labels to named claasses
+IDX_TO_CLASS = np.array(list(trainD.class_to_idx.keys()))
+CLASS_TO_IDX = dict(trainD.class_to_idx.items())
+
+from model import QuantWeightLeNet
+
+model = QuantWeightLeNet().to(device)
+
+def fixed_denormalize(image):
+    """ Restandartize images to [0, 255]"""
+    return image * 128 + 127.5
+
+from random import randrange
+
+criterion = torch.nn.TripletMarginLoss(margin=0.05, p=2).to(device=device)
+optimizer = torch.optim.Adam(model.parameters(),
+                                lr=1e-5,
+                                weight_decay=0.9)
+
+# Training
+for n_i in tqdm.trange(1000):
+    for i, (x, y) in enumerate(trainL, 1):
+
+        x = fixed_denormalize(x)
+        anchor_x = x[[0,2,4,6,8,10,12,14,16]]
+        anchor_y = y[[0,2,4,6,8,10,12,14,16]]
+        positive_x = x[[1,3,5,7,9,11,13,15,17]]
+        positive_y = y[[1,3,5,7,9,11,13,15,17]]
+
+        negative_index = [randrange(2,18),
+                          randrange(0,2) if randrange(0,2) else randrange(4, 18),
+                          randrange(0,4) if randrange(0,2) else randrange(6, 18),
+                          randrange(0,6) if randrange(0,2) else randrange(8, 18),
+                          randrange(0,8) if randrange(0,2) else randrange(10, 18),
+                          randrange(0,10) if randrange(0,2) else randrange(12, 18),
+                          randrange(0,12) if randrange(0,2) else randrange(14, 18),
+                          randrange(0,14) if randrange(0,2) else randrange(16, 18),
+                          randrange(0,16)]
+
+        negative_x = x[negative_index]
+        negative_y = y[negative_index]
+
+        anchor_embed = model(anchor_x.to(device))
+        positive_embed = model(positive_x.to(device))
+        negative_embed = model(negative_x.to(device))
+        # print(anchor_embed[0])
+
+        loss = criterion(anchor_embed, positive_embed, negative_embed)
+        print(n_i, ": ", loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+
+
